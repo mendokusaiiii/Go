6e90f3ffc6ed7805fe882d67a33888565f1eb70e
Victor Cabral <victor_cabral_o@hotmail.com>
2023-07-11 12:41:09 -0300

    chore: types

--- learning.go
+++ learning.go
@@ -1,12 +1,12 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	name := "Victor"
-	number := 25
+// func main() {
+// 	name := "Victor"
+// 	number := 25
 
-	fmt.Println("Hello World!")
-	fmt.Println((name))
-	fmt.Printf("%T", number)
-}
+// 	fmt.Println("Hello World!")
+// 	fmt.Println((name))
+// 	fmt.Printf("%T", number)
+// }
