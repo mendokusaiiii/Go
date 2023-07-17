cc20303a72a0e8fa39da0c8d2bf3cfee4c3e2adb
Victor Cabral <victor_cabral_o@hotmail.com>
2023-07-17 11:50:16 -0300

    chore: GO Variables

---
+++ 2- Variables/variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var variable1 string = "Variable 1"
+	variable2 := "Variable 2"
+
+	const cons1 string = "cons"
+
+	fmt.Println(variable1)
+	fmt.Println(variable2)
+	fmt.Println(cons1)
+}
